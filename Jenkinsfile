087ca2c99b347591a3eaf73306a640e84940c08e
Raghuram <raghuram@bicsglobal.com>
2023-10-26 20:51:33 +0530

    initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,27 @@
+pipeline {
+  agent any
+  environment {
+     registryUrl ="https://7tiuxysa.c1.gra9.container-registry.ovh.net"
+  }
+  stages{
+	  stage('building Docker Image') {
+	    steps {
+		    script {
+            sh 'docker build -t .'
+            sh "docker tag materialdashboard:latest ${registryUrl}/mydemoproject/harborimg:${env.BUILD_NUMBER}"
+            sh "docker rmi materialdashboard:latest"
+
+		    }
+	    }
+	  }
+
+    stage('Upload Image to Harbor registry') {
+      steps{   
+        script {
+          sh '''echo $DOCKER_CREDENTIALS_PSW | docker login $registryUrl -u 'DOCKER_CREDENTIALS_USER' --password-stdin'''
+          sh "docker image push ${registryUrl}/mydemoproject/harborimg:${env.BUILD_NUMBER}"
+        }
+      }
+    }
+  }  
+}
---
+++ build/precache-manifest.5524ecfc9b2afc739bfa15040396a437.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1062b8c87419185f348f9a18569a9913",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a10792a4c1cd54c3299d",
+    "url": "./static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "94df8d415b5992995038",
+    "url": "./static/css/main.994cfc1b.chunk.css"
+  },
+  {
+    "revision": "a10792a4c1cd54c3299d",
+    "url": "./static/js/2.ece36c39.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "./static/js/2.ece36c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94df8d415b5992995038",
+    "url": "./static/js/main.b96c0a4b.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "./static/js/main.b96c0a4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df50d4fc19e745850468",
+    "url": "./static/js/runtime-main.2ce2ec4b.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "./static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "./static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "./static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "./static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "./static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
